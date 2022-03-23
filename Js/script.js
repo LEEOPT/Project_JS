7423fb6852a5704e3dbcecd9618e7580aab75a15
Dmytro Korchevnyi <dkorchevnyi@gmail.com>
2022-03-23 11:09:56 +0200

    first commit

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("vam e 18" , "");
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt("what is your name?",""); 
+// answers[1] = prompt("what is your surname?",""); 
+// answers[2] = prompt("what is your age?",""); 
+
+// const category = 'toys';
+
+// console.log(`http//someurl.com/${category}/5`);
+
+const user = 'Dima';
+
+alert(`pryvit ${user}`);
+
+
