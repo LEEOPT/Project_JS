427be4a41615c4ec7688e7469a26d010a6202f0d
leeopt iphone <dkorchevnyi@gmail.com>
2022-03-24 11:47:07 +0200

    Vykonana druga zadacha

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,11 @@
-"use strict";
+ "use strict";
 
 const numberOfFilms = +prompt('скільки фільмів ви переглянули','');
 
-
-
+const personalMovieDB =  {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  geners: [],
+  privat: false
+};
